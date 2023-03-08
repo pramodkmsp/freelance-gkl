7c2f0caafa8ad5b0ddc2e91af3652ab50b77f907
kgp-india-private-limited <kgpindiaprivatelimited@gmail.com>
2023-03-08 18:30:16 +0530

    US002: Form validation completed for login and register page

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl, FormControl } from '@angular/forms';
+import Validation from '../must-match.validator';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group(
+      {
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['',[Validators.required,Validators.minLength(6),Validators.maxLength(40)]],
+        confirmPassword: ['', Validators.required],
+        mobile: ['', Validators.required],
+        gender: ['male'],
+      },
+      {
+        validators: [Validation.match('password', 'confirmPassword')],
+      }
+    );
   }
 
+  get f(): { [key: string]: AbstractControl } {
+    return this.registerForm.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    console.log(JSON.stringify(this.registerForm.value, null, 2));
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.registerForm.reset();
+  }
 }
